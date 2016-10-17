d978fd57b78e7c62d5396d9093302b3dc4ded369
t-gabor <gabor.tanka@gmail.com>
2016-10-17 10:28:48 +0200

    Use supertest to test the server.

--- test/test-server.js
+++ test/test-server.js
@@ -1,7 +1,6 @@
 const Server = require("../server/server");
-const assert = require("assert");
 const http = require("http");
-const fetch = require("node-fetch");
+const request = require("supertest");
 
 describe("#/api/control", () => {
     const server = new Server({
@@ -14,75 +13,47 @@ describe("#/api/control", () => {
     });
 
     it("should respond 404 to GET", (done) => {
-        fetch("http://localhost:3000/api/control")
-            .then((res) => {
-                assert.equal(404, res.status);
-                done();
-            })
-            .catch((err) => {
-                done(err);
-            });
+        request(server.app)
+            .get("/api/control")
+            .expect(404, done);
     });
 
     it("should respond 400 to marlformed request", (done) => {
-        fetch("http://127.0.0.1:3000/api/control", { method: "POST", body: "a=1" })
-            .then((res) => {
-                assert.equal(400, res.status);
-                done();
-            })
-            .catch((err) => {
-                done(err);
-            });
+        request(server.app)        
+            .post("/api/control")
+            .set("Content-Type", "application/json")
+            .send("a=1")
+            .expect(400, done);
     });
 
     function postState(state) {
-        return fetch("http://127.0.0.1:3000/api/control", {
-            method: "POST",
-            body: `{"state": "${state}"}`,
-            headers: {
-                "Content-Type": "application/json",
-            } });
+        return request(server.app)        
+            .post("/api/control")
+            .set("Content-Type", "application/json")
+            .send(`{"state": "${state}"}`);
     }
 
     it("should respond 200 to open request", (done) => {
         postState("open")
-            .then((res) => {
-                assert.equal(200, res.status);
-                done();
-            })
-            .catch((err) => {
-                done(err);
-            });
+            .expect(200, done);
     });
 
     it("should respond 200 to close request", (done) => {
         postState("close")
-            .then((res) => {
-                assert.equal(200, res.status);
-                done();
-            })
-            .catch((err) => {
-                done(err);
-            });
+            .expect(200, done);
     });
 
     it("should raise control:open event", (done) => {
         server.app.once("control:open", () => {
             done();
         });
-        postState("open")
-            .catch((err) => {
-                done(err);
-            });
+        postState("open").end();
     });
 
     it("should raise control:close event", (done) => {
         server.app.once("control:close", () => {
             done();
         });
-        postState("close")
-            .catch((err) => {
-                done(err);
-            });
+        postState("close").end();
     });
 });
