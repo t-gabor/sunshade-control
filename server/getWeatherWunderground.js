a75e894488f650d6c779a43a2e5a57780eec6724
t-gabor <gabor.tanka@gmail.com>
2016-11-04 13:40:45 +0100

    Get weather from wunderground.com.

---
+++ server/getWeatherWunderground.js
@@ -0,0 +1,26 @@
+const Wunderground = require("wundergroundnode");
+const settings = require("../config/wunderground.json");
+
+const wunderground = new Wunderground(settings.key);
+
+function extractWeatherData(data) {
+    const co = data.current_observation;
+    return {
+        windSpeed: Math.max(co.wind_kph, co.wind_gust_kph, data.hourly_forecast[0].wspd.metric),
+        temperature: co.temp_c,
+        summary: co.weather,
+        weatherCode: data.hourly_forecast[0].icon,
+        precipIntensity: co.precip_1hr_metric,
+        observationTime: co.observation_epoch
+    };
+}
+
+module.exports = (cb) => {
+    wunderground.conditions().hourlyForecast().request(settings.location, (error, response) => {
+        if (error) {
+            cb(error);
+        } else {
+            cb(null, extractWeatherData(response));
+        }
+    });
+};
